eadf1e91c57f43fe200e6120e24a1d70887b5734
Filip Hanik <fhanik@pivotal.io>
2016-09-15 08:51:42 -0700

    Initial setup for deployment script

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+git submodule sync
+git submodule foreach --recursive "git submodule sync; git clean -d --force --force"
+git submodule update --init --recursive --force
+
+cd uaa
+./gradlew clean assemble
+
+cd ..
+
+WAR_PATH=`find $PWD/uaa/uaa/build/libs/ -name '*.war'`
+echo $WAR_PATH
+
+cf api --skip-ssl-validation https://api.identity.cf-app.com
+cf login -o system -s openid-connect-server -u admin
+cf push -f manifest.yml -p $WAR_PATH
